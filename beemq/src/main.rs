61cc9db0f54fea3c8beabcd9ab994f8dab2046c1
Alberto <albertovalleflores99@gmail.com>
2024-05-25 16:41:17 -0700

    Refactor|Add: use workspace and move Cargo.toml to parent directory

--- beemq/src/main.rs
+++ beemq/src/main.rs
@@ -1,3 +1,10 @@
+use beemq_codec;
+
 fn main() {
-    println!("Hello, world!");
+    let num_one = 10;
+    let num_two = 5;
+    println!(
+        "Hello, world! {num_one} plus {num_two} is {}!",
+        beemq_codec::add(num_one, num_two)
+    );
 }
